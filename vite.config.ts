8a7a6afcd5755d10b89e3d6191b45702bfea8a33
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-08-16 08:30:56 +0700

    🛠️ Fix the deploy error again

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,26 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
-    const config = {
+    if (command === 'serve') {
+        return {
+            plugins: [svelte()],
+            resolve: {
+                alias: {
+                    '@config': path.resolve(__dirname, './src/config.ts'),
+                    '@components': path.resolve(__dirname, './src/lib/components/index.ts'),
+                    '@languages': path.resolve(__dirname, './src/lib/languages/index.ts'),
+                    '@lib': path.resolve(__dirname, './src/lib'),
+                    '@stores': path.resolve(__dirname, './src/lib/stores'),
+                    '@styles': path.resolve(__dirname, './src/lib/styles'),
+                    '@assets': path.resolve(__dirname, './src/lib/assets'),
+                },
+            },
+        }
+    }
+
+    return {
         plugins: [svelte()],
         base: '/introduction-lecture-2023-ganjil/',
         resolve: {
@@ -18,9 +36,4 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
             },
         },
     }
-    if (command === 'build') {
-        config.base = '/introduction-lecture-2023-ganjil/'
-    }
-
-    return config;
 })
