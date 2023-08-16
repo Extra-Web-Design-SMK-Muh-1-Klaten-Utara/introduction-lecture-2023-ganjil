23f1006f54fb01e49bc1c301d3fa057572f85c6a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-08-16 08:22:32 +0700

    🛠️ Fix deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,25 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [svelte()],
-	resolve: {
-		alias: {
-			'@config': path.resolve(__dirname, './src/config.ts'),
-			'@components': path.resolve(__dirname, './src/lib/components/index.ts'),
-			'@languages': path.resolve(__dirname, './src/lib/languages/index.ts'),
-			'@lib': path.resolve(__dirname, './src/lib'),
-			'@stores': path.resolve(__dirname, './src/lib/stores'),
-			'@styles': path.resolve(__dirname, './src/lib/styles'),
-			'@assets': path.resolve(__dirname, './src/lib/assets'),
-		},
-	},
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    const config = {
+        plugins: [svelte()],
+        base: '/introduction-lecture-2023-ganjil/',
+        resolve: {
+            alias: {
+                '@config': path.resolve(__dirname, './src/config.ts'),
+                '@components': path.resolve(__dirname, './src/lib/components/index.ts'),
+                '@languages': path.resolve(__dirname, './src/lib/languages/index.ts'),
+                '@lib': path.resolve(__dirname, './src/lib'),
+                '@stores': path.resolve(__dirname, './src/lib/stores'),
+                '@styles': path.resolve(__dirname, './src/lib/styles'),
+                '@assets': path.resolve(__dirname, './src/lib/assets'),
+            },
+        },
+    }
+    if (command === 'build') {
+        config.base = '/introduction-lecture-2023-ganjil/'
+    }
+
+    return config;
 })
